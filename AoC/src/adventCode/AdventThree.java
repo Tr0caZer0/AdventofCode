dd740d436b94b0ea397bcded42467a706758ecb8
Bastian Åström <ba222ji@student.lnu.se>
2023-12-04 23:54:09 +0200

    First commit adventThree. Added file reader and split rows into char arrays.

--- AoC/src/adventCode/AdventThree.java
+++ AoC/src/adventCode/AdventThree.java
@@ -1,5 +1,44 @@
 package adventCode;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AdventThree {
- 
+	
+	public static void main(String[] args) {
+		readDoc();
+	}
+
+	private static void readDoc() {
+		StringBuilder charToInt = new StringBuilder();
+		
+		try(BufferedReader readData = new BufferedReader(new FileReader("adventThird.txt") )){
+			String details;
+			while((details = readData.readLine()) != null){
+				
+				char[] splitDetails = details.toCharArray();
+				
+				for(int i = 0; i < splitDetails.length; i++) {
+					
+					if(Character.isDigit(splitDetails[i])) {
+						
+						//Append to build integer
+						//Save specific index location
+						
+					}else if(splitDetails[i] != '.') {
+						//Save specific index location
+					}
+					
+					
+					
+				}
+				System.out.println("");
+			}
+			
+		}catch(IOException e) {
+			System.out.println("An Error occured" + e);
+		}
+		
+	}
 }
