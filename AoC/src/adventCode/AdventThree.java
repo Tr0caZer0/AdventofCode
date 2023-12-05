6b3decedc814a97fbbf746887254f23bc85dff11
Bastian Åström <ba222ji@student.lnu.se>
2023-12-05 21:45:42 +0200

    Third advent. Cretaed two methods to iterate char array. one looks for digits and two looks for symbols.

--- AoC/src/adventCode/AdventThree.java
+++ AoC/src/adventCode/AdventThree.java
@@ -3,41 +3,80 @@ package adventCode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdventThree {
+	
+	public static List<Integer> indexCheckOne = new ArrayList<>();
+	public static List<Integer> indexCheckTwo = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		readDoc();
 	}
 
 	private static void readDoc() {
-		StringBuilder charToInt = new StringBuilder();
+		
+		int counter = 0;
 		
 		try(BufferedReader readData = new BufferedReader(new FileReader("adventThird.txt") )){
 			String details;
 			while((details = readData.readLine()) != null){
+				counter++;
 				
 				char[] splitDetails = details.toCharArray();
 				
-				for(int i = 0; i < splitDetails.length; i++) {
-					
-					if(Character.isDigit(splitDetails[i])) {
-						
-						//Append to build integer
-						//Save specific index location
-						
-					}else if(splitDetails[i] != '.') {
-						//Save specific index location
-					}
-					
-					
-					
+				if(counter%2 == 0) {
+					catchIndexOne(splitDetails);
+				}else {
+					catchIndexTwo(splitDetails);
 				}
+				
 				System.out.println("");
 			}
 			
 		}catch(IOException e) {
 			System.out.println("An Error occured" + e);
+		}
+		
+	}
+
+	private static void catchIndexTwo(char[] splitDetails) {
+		StringBuilder charToInt = new StringBuilder();
+		
+		for(int i = 0; i < splitDetails.length; i++) {
+			
+			if(Character.isDigit(splitDetails[i])) {
+				
+				//Append to build integer
+				//Save specific index location
+				
+			}else if(splitDetails[i] != '.') {
+				//Save specific index location
+			}
+			
+			
+			
+		}
+		
+	}
+
+	private static void catchIndexOne(char[] splitDetails) {
+		StringBuilder charToInt = new StringBuilder();
+		
+		for(int i = 0; i < splitDetails.length; i++) {
+			
+			if(Character.isDigit(splitDetails[i])) {
+				
+				//Append to build integer
+				//Save specific index location
+				
+			}else if(splitDetails[i] != '.') {
+				//Save specific index location
+			}
+			
+			
+			
 		}
 		
 	}
